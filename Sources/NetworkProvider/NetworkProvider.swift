b2df627a3dbdbbd3166427f800bf195ae14789d0
vishal kalola <vkalola@een.com>
2022-10-11 21:38:06 +0200

    Added public access

--- Sources/NetworkProvider/NetworkProvider.swift
+++ Sources/NetworkProvider/NetworkProvider.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-struct NetworkProvider: NetworkProviderProtocol {
+public struct NetworkProvider: NetworkProviderProtocol {
     
     private let session: SessionProtocol
     
-    init(session: SessionProtocol = Session()) {
+    public init(session: SessionProtocol = Session()) {
         self.session = session
     }
     
-    func request<T>(dataType: T.Type, service: NetworkService, onQueue: DispatchQueue, completion: @escaping (Result<T, Error>) -> Void) where T: Decodable {
+    public func request<T>(dataType: T.Type, service: NetworkService, onQueue: DispatchQueue, completion: @escaping (Result<T, Error>) -> Void) where T: Decodable {
         let task = session.dataTask(service.urlRequest, dataType: dataType) { result in
             onQueue.async {
                 completion(result)
